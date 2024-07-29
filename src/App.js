b770c009f999f526f3ffa1816be85cff7b9eedf8
Despega tu imperio digital <kinder@MacBook-Air-de-Kinder.local>
2024-07-29 13:54:23 -0400

    subiendo json de prueba

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/" element={<SearchBar />} />
         </Routes>
       </div>
     </Router>
